fde9d83c968f0e7bed08d2b317e1cab9f98aafbf
Anton Gorynya <antongorynya1@gmail.com>
2025-05-24 20:28:59 +0300

    add req readme

--- send_simple_packets.py
+++ send_simple_packets.py
@@ -1,33 +1,36 @@
 import argparse
+import time
 from scapy.layers.l2 import Ether, Dot1Q
 from scapy.layers.inet import IP, TCP, UDP, ICMP
 from scapy.all import Padding, sendpfast
-from socket import gaierror
 
 
-def generate_packets(iface, eth_src, eth_dst, ip_src, ip_dst, transport_proto, tagged_vlan=0, port_src=[1], port_dst=[1], count=1):
-    eth = Ether(src=eth_src, dst=eth_dst)
-    ip = IP(src=ip_src, dst=ip_dst)
-    if transport_proto == 'tcp':
-        transport = TCP(sport=port_src, dport=port_dst)
-    if transport_proto == 'udp':
-        transport = UDP(sport=port_src, dport=port_dst)
-    if transport_proto == 'icmp':
-        transport = ICMP()
-    packet = eth / Dot1Q(vlan=tagged_vlan) / ip / transport if tagged_vlan else eth / ip / transport
+def add_padding(packet):
+    if len(packet) > 59:
+        return packet
     pad_len = 60 - len(packet)
     pad = Padding()
-    pad.load = '\\x00' * pad_len
+    pad.load = b"\x00" * pad_len
     packet = packet/pad
     return packet
 
 
-def send_packets(iface, eth_src, eth_dst, ip_src, ip_dst, transport_proto, tagged_vlan=0, port_src=[1], port_dst=[1], count=1):
-    packets = []
-    for transport in transport_proto:
-        packets += generate_packets(iface, eth_src, eth_dst, ip_src, ip_dst, transport, tagged_vlan, port_src, port_dst, count)
-    print(f'Total:{len(packets)}')
-    sendpfast(packets, mbps=10, count=count, parse_results=True, iface=iface)
+def create_tcp_packets(eth_src, eth_dst, ip_src, ip_dst, tagged_vlan=0, port_src=[1], port_dst=[1]):
+    eth = Ether(src=eth_src, dst=eth_dst) / Dot1Q(vlan=tagged_vlan) if tagged_vlan else Ether(src=eth_src, dst=eth_dst)
+    packets = add_padding(eth / IP(src=ip_src, dst=ip_dst) / TCP(sport=port_src, dport=port_dst))
+    return packets
+
+
+def create_udp_packets(eth_src, eth_dst, ip_src, ip_dst, tagged_vlan=0, port_src=[1], port_dst=[1]):
+    eth = Ether(src=eth_src, dst=eth_dst) / Dot1Q(vlan=tagged_vlan) if tagged_vlan else Ether(src=eth_src, dst=eth_dst) 
+    packets = add_padding(eth / IP(src=ip_src, dst=ip_dst) / UDP(sport=port_src, dport=port_dst))
+    return packets
+
+
+def create_icmp_packets(eth_src, eth_dst, ip_src, ip_dst, tagged_vlan=0):
+    eth = Ether(src=eth_src, dst=eth_dst) / Dot1Q(vlan=tagged_vlan) if tagged_vlan else Ether(src=eth_src, dst=eth_dst)   
+    packets = add_padding(eth / IP(src=ip_src, dst=ip_dst)  / ICMP())
+    return packets
 
 
 def create_parser():
@@ -38,32 +41,66 @@ def create_parser():
     parser.add_argument("--mac_src", help="list mac src", nargs="+")
     parser.add_argument("--mac_dst", help="list mac dst", nargs="+")
     parser.add_argument("--vlan", help="tagged vlan", default=0, type=int)
+    parser.add_argument("--speed", help="Speed in Mbits per second",
+                        default=10, type=int)
     parser.add_argument("--ip_src", help="list ip src", nargs="+")
     parser.add_argument("--ip_dst", help="list ip src", nargs="+")
-    parser.add_argument("--tcp", dest="transport", 
-                        action="append_const", const="tcp")
-    parser.add_argument("--udp", dest="transport", 
-                        action="append_const", const="udp")
-    parser.add_argument("--icmp", dest="transport", 
-                        action="append_const", const="icmp")
-    parser.add_argument("--port_src", help="list port src", 
+    parser.add_argument("--tcp", action="store_true")
+    parser.add_argument("--udp", action="store_true")
+    parser.add_argument("--icmp", action="store_true")
+    parser.add_argument("--port_src", help="list port src",
                         nargs="+", type=int, default=[1])
-    parser.add_argument("--port_dst", help="list port src", 
+    parser.add_argument("--port_dst", help="list port src",
                         nargs="+", type=int, default=[1])
+    parser.add_argument("--count", help="Total repeating", default=1, type=int)
     return parser
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     parser = create_parser()
     args = parser.parse_args()
-
-    try:
-        send_packets(
-            args.iface, args.mac_src, args.mac_dst, 
-            args.ip_src, args.ip_dst, args.transport, 
-            tagged_vlan=args.vlan, 
-            port_src=args.port_src, port_dst=args.port_dst, 
-            count=1
+    packets = []
+    if args.tcp:
+        packets.append(
+            create_tcp_packets(
+                args.mac_src,
+                args.mac_dst,
+                args.ip_src,
+                args.ip_dst,
+                tagged_vlan=args.vlan,
+                port_src=args.port_src,
+                port_dst=args.port_dst
             )
-    except gaierror as e: print(e)
-    except ValueError: print("Please check input params")
+        )
+    if args.udp:
+        packets.append(
+            create_udp_packets(
+                args.mac_src,
+                args.mac_dst,
+                args.ip_src,
+                args.ip_dst,
+                tagged_vlan=args.vlan,
+                port_src=args.port_src,
+                port_dst=args.port_dst
+            )
+        )
+    if args.icmp:
+        packets.append(
+            create_tcp_packets(
+                args.mac_src,
+                args.mac_dst,
+                args.ip_src,
+                args.ip_dst,
+                tagged_vlan=args.vlan,
+            )
+        )
+
+    sendpfast(
+        packets,
+        mbps=args.speed,
+        count=args.count,
+        parse_results=True,
+        iface=args.iface
+    )
+    print("--- %s seconds ---" % (time.time() - start_time))
