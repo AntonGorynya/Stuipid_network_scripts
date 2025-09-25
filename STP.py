cb889298c0df787af484d531073399a0bf3274d5
Anton Gorynya <antongorynya1@gmail.com>
2025-09-25 21:52:22 +0300

    generate_pvst_tcn

--- STP.py
+++ STP.py
@@ -23,8 +23,8 @@ class STP_OriginatingVLAN(Packet):
     ]
 
 
-class STP_topology_change_bdpu(Packet):
-    name = "Spanning Tree Protocol Topology Change"
+class STP_TCN(Packet):
+    name = "Spanning Tree Protocol Topology Change Notification"
     fields_desc = [
         ShortField("proto", 0),
         ByteField("version", 0),
@@ -69,34 +69,12 @@ def generate_stp_bpdu(
 
 
 @padding
-def generate_stp_tc_bpdu(bridge_mac="00:00:00:00:00:01"):
-    payload = LLC(dsap=0x42, ssap=0x42, ctrl=3) / STP_topology_change_bdpu()
+def generate_stp_tcn(bridge_mac="00:00:00:00:00:01"):
+    payload = LLC(dsap=0x42, ssap=0x42, ctrl=3) / STP_TCN()
     return (
         Ether(src=bridge_mac, dst="01:80:c2:00:00:00", type=len(payload))
         / payload
     )
-
-
-def generate_mstp_configuration_digest(instance_meta):
-    """
-    На вход подается словарь вида:
-    {
-        instance_number: [vla_idn...]
-    }
-    Пример для всех VLAN в 1ом instance
-    {
-        1: [i for i in range(1, 4095)]
-    }
-    Вывод: ac36177f50283cd4b83821d8ab26de62
-    """
-    mst_configuration_table = [b'\x00\x00']*4096
-    for instance, vlan_range in instance_meta.items():
-        for vlan_id in vlan_range:
-            mst_configuration_table[vlan_id] = struct.pack('>H', instance)
-
-    message = b''.join(mst_configuration_table)
-    hmac_object = hmac.new(MSTP_KEY, message, hashlib.md5)
-    return hmac_object.hexdigest()
 
 
 def generate_pvst_bpdu(
@@ -125,7 +103,41 @@ def generate_pvst_bpdu(
     )
 
 
+@padding
+def generate_pvst_tcn(bridge_mac="00:00:00:00:00:01", vlan_id=10):
+    payload = (
+        LLC(dsap=0xAA, ssap=0xAA, ctrl=3)
+        / SNAP(OUI=0x00000C, code=0x010B) 
+        / STP_TCN()
+        / STP_OriginatingVLAN(vlan=vlan_id)
+    )
+    return (
+        Ether(src=bridge_mac, dst="01:00:0c:cc:cc:cd", type=0x8100)
+        / Dot1Q(prio=5, vlan=vlan_id, type=len(payload))
+        / payload
+    )
 
+
+def generate_mstp_configuration_digest(instance_meta):
+    """
+    На вход подается словарь вида:
+    {
+        instance_number: [vla_idn...]
+    }
+    Пример для всех VLAN в 1ом instance
+    {
+        1: [i for i in range(1, 4095)]
+    }
+    Вывод: ac36177f50283cd4b83821d8ab26de62
+    """
+    mst_configuration_table = [b'\x00\x00']*4096
+    for instance, vlan_range in instance_meta.items():
+        for vlan_id in vlan_range:
+            mst_configuration_table[vlan_id] = struct.pack('>H', instance)
+
+    message = b''.join(mst_configuration_table)
+    hmac_object = hmac.new(MSTP_KEY, message, hashlib.md5)
+    return hmac_object.hexdigest()
 
 
 def handle_bpdu(pkt, iface1, iface2, cost1, cost2, bridge_mac1='11:11:11:11:11:11', bridge_mac2='11:11:11:11:11:11', port_prio1=0x8000, port_prio2=0x8000, port_num1=1, port_num2=2):
