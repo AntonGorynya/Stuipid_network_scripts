f1142c404d03c01bc31a00ec601a6efc2d1f320b
Anton Gorynya <antongorynya1@gmail.com>
2025-09-25 21:10:34 +0300

    add generate_stp_tc_bpdu

--- STP.py
+++ STP.py
@@ -1,5 +1,5 @@
 from scapy.layers.l2 import Ether, Dot1Q, LLC, SNAP, STP
-from scapy.all import hexdump, Packet, ShortField, ByteField, sniff, sendp
+from scapy.all import hexdump, Packet, ShortField, ByteField, sniff, sendp, Padding
 from functools import partial
 from time import sleep
 import hmac
@@ -12,14 +12,69 @@ TCA = 0x80
 
 MSTP_KEY = bytes.fromhex('13AC06A62E47FD51F95D2BA243CD0346') # 802.1Q-2022
 
+
 class STP_OriginatingVLAN(Packet):
     name = "STP Originating VLAN TLV"
     fields_desc = [
         ByteField("pad", 0x00),  # у сisco 3 байта 0х00
-        ShortField("type", 0x0000), 
+        ShortField("type", 0x0000),
         ShortField("length", 0x0002),
         ShortField("vlan", 1)
     ]
+
+
+class STP_topology_change_bdpu(Packet):
+    name = "Spanning Tree Protocol Topology Change"
+    fields_desc = [
+        ShortField("proto", 0),
+        ByteField("version", 0),
+        ByteField("bpdutype", 0x80),
+    ]
+
+
+def padding(func):
+    def wrapper():
+        if len(func()) > 59:
+            return func()
+        pad_len = 60 - len(func())
+        pad = Padding()
+        pad.load = b"\x00" * pad_len
+        packet = func()/pad
+        return packet
+    return wrapper
+
+
+@padding
+def generate_stp_bpdu(
+    bridge_mac="00:00:00:00:00:01", root_mac="00:00:00:00:00:01",
+    bridge_prio=32768, root_prio=32768, vlan_id=1, path_cost=0,
+    port_priority=0x8000, port_num=1, flags=0x00):
+    payload = (
+        LLC(dsap=0x42, ssap=0x42, ctrl=3)
+        / STP(
+            rootid=root_prio + vlan_id,
+            rootmac=root_mac,
+            pathcost=path_cost,
+            bridgeid=bridge_prio + vlan_id,
+            bridgemac=bridge_mac,
+            portid=port_priority+port_num,
+            bpduflags=flags,
+            age=0.0,
+        )
+    )
+    return (
+        Ether(src=bridge_mac, dst="01:80:c2:00:00:00", type=len(payload))
+        / payload
+    )
+
+
+@padding
+def generate_stp_tc_bpdu(bridge_mac="00:00:00:00:00:01"):
+    payload = LLC(dsap=0x42, ssap=0x42, ctrl=3) / STP_topology_change_bdpu()
+    return (
+        Ether(src=bridge_mac, dst="01:80:c2:00:00:00", type=len(payload))
+        / payload
+    )
 
 
 def generate_mstp_configuration_digest(instance_meta):
@@ -70,27 +125,7 @@ def generate_pvst_bpdu(
     )
 
 
-def generate_stp_bpdu(
-    bridge_mac="00:00:00:00:00:01", root_mac="00:00:00:00:00:01",
-    bridge_prio=32768, root_prio=32768, vlan_id=1, path_cost=0,
-    port_priority=0x8000, port_num=1, flags=0x00):
-    payload = (
-        LLC(dsap=0x42, ssap=0x42, ctrl=3)
-        / STP(
-            rootid=root_prio + vlan_id,
-            rootmac=root_mac,
-            pathcost=path_cost,
-            bridgeid=bridge_prio + vlan_id,
-            bridgemac=bridge_mac,
-            portid=port_priority+port_num,
-            bpduflags=flags,
-            age=0.0,
-        )
-    )
-    return (
-        Ether(src=bridge_mac, dst="01:80:c2:00:00:00", type=len(payload))
-        / payload 
-    )
+
 
 
 def handle_bpdu(pkt, iface1, iface2, cost1, cost2, bridge_mac1='11:11:11:11:11:11', bridge_mac2='11:11:11:11:11:11', port_prio1=0x8000, port_prio2=0x8000, port_num1=1, port_num2=2):
@@ -127,3 +162,5 @@ def change_root(
             port_num1=port_num1, port_num2=port_num2
         ), store=False
     )
+
+print(len(generate_stp_bpdu()))
